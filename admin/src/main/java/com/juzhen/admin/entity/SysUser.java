c4fdcf678db7ce461cb730028bc563ce4eb33412
ChangfengHU <faker_322@outlook.com>
2021-05-27 02:17:53 +0800

    first commit

---
+++ admin/src/main/java/com/juzhen/admin/entity/SysUser.java
@@ -0,0 +1,61 @@
+package com.juzhen.admin.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * <p>
+ * 系统用户
+ * </p>
+ *
+ * @author huchangfeng
+ * @since 2021-05-26
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+public class SysUser implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "user_id", type = IdType.AUTO)
+    private Long userId;
+
+    private String username;
+
+    private String password;
+
+    private String salt;
+
+    private String email;
+
+    private String mobile;
+
+    /**
+     * 状态  0：禁用   1：正常
+     */
+    private Integer status;
+
+    /**
+     * 类型 1 管理员  2 工厂 3销售
+     */
+    private Integer kind;
+
+    /**
+     * 创建者ID
+     */
+    private Long createUserId;
+
+    /**
+     * 创建时间
+     */
+    private LocalDateTime createTime;
+
+
+}
