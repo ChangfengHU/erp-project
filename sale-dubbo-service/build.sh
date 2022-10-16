1e2a6e4f379e54dfd62a0f369ac0ab9427c36b14
ChangfengHU <faker_322@outlook.com>
2022-10-16 14:11:56 +0800

    erp  基础环境单间完毕 dubbo服务调用 目前还有问题 ,不过可以先用thrift替换 后面再说

--- sale-dubbo-service/build.sh
+++ sale-dubbo-service/build.sh
@@ -1,5 +1,5 @@
 ##!/usr/env bash
-#mvn clean package
-#docker build -t  sale-service:latest .
-docker tag sale-edge-service:latest 42.192.76.49:81/erp-service/sale-service:1.1
-docker push 42.192.76.49:81/erp-service/sale-service:1.1
+mvn clean package
+docker build -t  sale-service:latest .
+docker tag sale-service:latest 42.192.76.49:81/erp-service/sale-service:latest
+docker push 42.192.76.49:81/erp-service/sale-service:latest
